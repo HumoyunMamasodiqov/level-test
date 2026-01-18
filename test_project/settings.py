6a768abc07d3f7404deee69c2863238b2de9970d
Humoyun Mamasodiqov <mushtariy3748@gmail.com>
2026-01-18 13:34:50 +0500

    first commit 11

--- test_project/settings.py
+++ test_project/settings.py
@@ -122,13 +122,11 @@ USE_TZ = True
 # ========================
 # STATIC FILES (ADMIN FIX)
 # ========================
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ========================
 # MEDIA FILES
 # ========================
